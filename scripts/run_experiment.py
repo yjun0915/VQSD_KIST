ed78030fd153e4a44ea7dbd99a8325edb84325fc
yjun0915 <kyjun09150916@gmail.com>
2026-03-01 15:34:16 +0900

    feat: data saving logic

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -20,7 +20,7 @@ overlap = s_parameter
 rho_list = get_rho_list(prepared_state_set)
 dim = len(prior_probability)
 
-results = pd.DataFrame(index=range(1, trial_num+1), columns=["success rate", "error rate", "failure rate"])
+raw_data = pd.DataFrame(index=range(1, trial_num+1), columns=["success rate", "error rate", "failure rate"])
 
 with timetagger_session(500, 50, 2, [0, 0]) as timetagger:
     with slm_session() as slm:
@@ -50,10 +50,10 @@ with timetagger_session(500, 50, 2, [0, 0]) as timetagger:
                 for vector_idx, vector in enumerate(vector_list):
                     optimal_measurements[f"M{np.mod(vector_idx + 1, dim)}"] = vector
 
-                results.loc[res_idx] = get_discrimination_rates(rho_list, optimal_measurements, prior_probability)
+                raw_data.loc[res_idx] = get_discrimination_rates(rho_list, optimal_measurements, prior_probability)
 
 from datetime import datetime
 now = datetime.now()
 timestamp = now.strftime("%Y%m%d_%H%M%S")
 file_name = f"experiment_results_{timestamp}.csv"
-results.to_csv(file_name)
+raw_data.to_csv(file_name)
--- scripts/run_simulation.py
+++ scripts/run_simulation.py
@@ -1,12 +1,28 @@
+import yaml
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from scipy.optimize import minimize
 
 from src.utils.quantum_states import *
 from src.theory.discriminator import *
-from src.hardware.tcspc_core import *
 
 
+# region parameter configuration
+with open("../config/params.yaml", "r", encoding="utf-8") as f:
+    config = yaml.safe_load(f)
+
+opt_method = config['optimization']['method']
+tol = config['optimization']['tol']
+rhobeg = eval(config['optimization']['rhobeg'])
+max_iter = int(float(config['optimization']['maxiter']))
+
+lambda_val = config['simulation']['lambda_val']
+
+columns = config['columns']
+# endregion
+
+# region toy example
 s_parameter = 0.75
 prior_probability = [0.5, 0.5, 0]
 prepared_state_set = np.array([
@@ -16,20 +32,20 @@ prepared_state_set = np.array([
 overlap = s_parameter
 rho_list = get_rho_list(prepared_state_set)
 dim = len(prior_probability)
-
-plt.figure(1)
+# endregion
 
 
 # region theory curve
+theory_df = pd.DataFrame(columns=columns['theory'])
 for fixed_rate in np.linspace(0, 1, 100):
     optimal_measurements = solve_sdp_bound(prepared_state_set, prior_probability, dim, fixed_rate)
 
     P_success, P_error, P_fail = get_discrimination_rates(rho_list, optimal_measurements, prior_probability)
+    row = [overlap, fixed_rate, P_success, P_error, P_fail]
 
-    plt.scatter(fixed_rate, P_success, color='dodgerblue')
-    plt.scatter(fixed_rate, P_error, color='firebrick')
-    plt.scatter(fixed_rate, P_fail, color='limegreen')
+    theory_df = pd.concat([theory_df, pd.DataFrame([row], columns=columns['theory'])], ignore_index=True)
 # endregion
+
 
 for fixed_rate in np.linspace(0, overlap, 50):
     obj = cobyla_objective
@@ -39,12 +55,12 @@ for fixed_rate in np.linspace(0, overlap, 50):
     result = minimize(
         obj,
         initial_parameter,
-        args=(prepared_state_set, prior_probability, dim, fixed_rate, 3),
-        method='COBYLA',
-        tol=0.01,
+        args=(prepared_state_set, prior_probability, dim, fixed_rate, lambda_val),
+        method=opt_method,
+        tol=tol,
         options={
-            'rhobeg': np.pi/2,
-            'maxiter': 1e3,
+            'rhobeg': rhobeg,
+            'maxiter': max_iter,
             'disp': False,
         }
     )
