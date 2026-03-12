4d688c62ba165dc315b3779a7a51f88021031696
yjun0915 <kyjun09150916@gmail.com>
2026-03-12 15:19:12 +0900

    refactor: experiment code refactoring

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -33,58 +33,45 @@ dim = minimize_params['dim']
 overlap = minimize_params['overlap']
 prior_probability = [1/(dim-1) for _ in range(dim-1)] + [0]
 prepared_state_set = np.hstack((prepared_state_d_dim(dim-1, overlap), [[0] for _ in range(dim-1)]))
-
 rho_list = get_rho_list(prepared_state_set)
 
 # region theory data
-theory_df = pd.DataFrame(columns=columns['theory'])
 theory_dir = Path(f"../data/theory/dim_{dim}")
 theory_dir.mkdir(parents=True, exist_ok=True)
 theory_filename = f"ov{overlap:.2f}.csv"
 theory_filepath = theory_dir / theory_filename
+theory_rows = []
 for fixed_rate in np.linspace(0, 1, 100):
     optimal_measurements = solve_sdp_bound(prepared_state_set, prior_probability, dim, fixed_rate)
     P_success, P_error, P_fail = get_discrimination_rates(rho_list, optimal_measurements, prior_probability)
-    new_row = [overlap, fixed_rate, P_success, P_error, P_fail]
-    theory_df = pd.concat([theory_df, pd.DataFrame([new_row], columns=columns['theory'])], ignore_index=True)
+    theory_rows.append([overlap, fixed_rate, P_success, P_error, P_fail])
+theory_df = pd.DataFrame(theory_rows, columns=columns['theory'])
 theory_df.to_csv(theory_filepath, index=False)
 # endregion
 
 
 start = time.time()
 # region experiment data
-raw_df = pd.DataFrame(columns=columns['raw data'])
-raw_history_df = pd.DataFrame([])
-
-current_time = datetime.now().strftime("%y%m%d_%H%M")
-raw_dir = Path(f"../data/experiment/dim_{dim}")
-raw_dir.mkdir(parents=True, exist_ok=True)
-
-raw_filename = f"{current_time}_ov{overlap:.2f}.csv"
-raw_history_filename = f"{current_time}_ov{overlap:.2f}_history.csv"
-
-raw_filepath = raw_dir / raw_filename
-raw_history_filepath = raw_dir / raw_history_filename
+dir = Path(f"../data/experiment/dim_{dim}")
+dir.mkdir(parents=True, exist_ok=True)
+filename = f"ov{overlap:.2f}.csv"
+filepath = dir / filename
+is_new_file = not filepath.exists()
 
 fixed_rates = minimize_params['q_points']
-best_lagrangians = np.full(len(fixed_rates), -np.inf)
-best_histories = [[] for _ in range(len(fixed_rates))]
-
 timetagger_config = config['devices']['timetagger']
-with timetagger_session(timetagger_config['cw'], timetagger_config['binwidth'], timetagger_config['n_value'], timetagger_config['delay']) as timetagger:
-    cw = config['devices']['timetagger']['cw']
-    with slm_session() as slm:
-        experiment = Experiment(timetagger, slm, prepared_state_set, dim)
+cw, binwidth, n_value, delay = timetagger_config['cw'], timetagger_config['binwidth'], timetagger_config['n_value'], timetagger_config['delay']
+with timetagger_session(cw, binwidth, n_value, delay) as timetagger:
+    with slm_session() as slms:
+        experiment = Experiment(timetagger, slms, prepared_state_set, dim)
         for trial in trange(minimize_params['trial'], desc="Trials"):
             for fr_idx, fixed_rate in enumerate(fixed_rates):
+
                 parameter_history = []
-
-
                 def tracking_objective(x, *args):
                     current_lagrangian = experiment.cobyla_objective(x, *args)
                     parameter_history.append(x.copy().tolist() + [float(-current_lagrangian)])
                     return current_lagrangian
-
 
                 initial_parameter = np.random.uniform(0, 2 * np.pi, size=((dim ** 2) - 1))
                 result = minimize(
@@ -95,11 +82,10 @@ with timetagger_session(timetagger_config['cw'], timetagger_config['binwidth'], 
                 )
                 lagrangian = -result.fun
 
+                raw_data = [[0 for __ in range(dim)] for _ in range(dim-1)]
                 vector_list = unitary_matrix(result.x, dim).T
-
-                temp_rate = np.zeros(shape=(dim - 1, dim))
                 for state_idx, state in enumerate(prepared_state_set):
-                    slm[0].imshow(experiment.state_holograms[str(state)])
+                    slms[0].imshow(experiment.state_holograms[str(state)])
                     for vector_idx, vector in enumerate(vector_list):
                         fields = generate_oam_superposition(
                             res=experiment.slm_config['res'],
@@ -112,7 +98,7 @@ with timetagger_session(timetagger_config['cw'], timetagger_config['binwidth'], 
                             measure=False
                         )
                         projection_hologram = encode_hologram(*fields, pixel_pitch=experiment.slm_config['pixel_pitch'], d=8, N_steps=8,  M=1, prepare=False, measure=True, save=False)
-                        slm[1].imshow(projection_hologram)
+                        slms[1].imshow(projection_hologram)
 
                         time.sleep(0.2)
 
@@ -121,33 +107,21 @@ with timetagger_session(timetagger_config['cw'], timetagger_config['binwidth'], 
                         B_channel_counts = np.sum(a=count_data, axis=1)[1]
                         coincidence_data = np.sum(a=count_data, axis=1)[2]
                         coincidence_data -= max(0, A_channel_counts * B_channel_counts * cw * 1e-12)
-                        temp_rate[state_idx][vector_idx] += prior_probability[state_idx] * coincidence_data
+                        raw_data[state_idx][vector_idx] += prior_probability[state_idx] * coincidence_data
 
-                P_success = np.trace(temp_rate)
-                P_fail = np.sum(temp_rate[:, -1])
-                P_error = np.sum(temp_rate) - (P_success + P_fail)
-                new_row = [overlap, trial, fixed_rate, P_success, P_error, P_fail, lagrangian, lambda_val, opt_config['method'], result.nfev]
-                raw_df = pd.concat([raw_df, pd.DataFrame([new_row], columns=columns['sim data'])], ignore_index=True)
-
-                if lagrangian > best_lagrangians[fr_idx]:
-                    best_histories[fr_idx] = parameter_history
-                    best_lagrangians[fr_idx] = lagrangian
-
-history_dict = {f"{fixed_rates[i]:.3f}": pd.Series(best_histories[i]) for i in range(len(fixed_rates))}
-sim_history_df = pd.DataFrame(history_dict)
-sim_history_df.columns = pd.MultiIndex.from_product([["fixed rate"], sim_history_df.columns])
-
-raw_df.to_csv(raw_filepath, index=False)
-sim_history_df.to_csv(raw_history_filepath, index=False)
+                current_time = datetime.now().strftime("%y%m%d%H%M%S")
+                new_row_df = pd.DataFrame([{
+                    'timestamp': current_time,
+                    'optimizer': config['minimize']['optimizer'],
+                    'lambda_val': lambda_val,
+                    'fixedrate': fixed_rate,
+                    'history': list(map(list, zip(*parameter_history))),
+                    'raw_data': raw_data
+                }])
+                new_row_df.to_csv(filepath, mode='a', index=False, header=is_new_file, encoding='utf-8-sig')
+                is_new_file = False
 # endregion
 end = time.time()
 elapsed_time_raw = end - start
 minutes, seconds = divmod(elapsed_time_raw, 60)
 time_str = f"{int(minutes)}m {seconds:.2f}s"
-
-best_idx = raw_df['lagrangian'].idxmax()
-best_fixed_rate = raw_df.loc[best_idx, 'fixed rate']
-max_P_succ = raw_df.loc[best_idx, 'success rate']
-theory_P_succ = theory_df.iloc[(theory_df['fixed rate'] - best_fixed_rate).abs().argsort()[:1]]['success rate'].values[0]
-avg_lag = raw_df['lagrangian'].mean()
-send_message(dim, overlap, opt_config['method'], minimize_params['lambda_val'], max_P_succ, theory_P_succ, avg_lag, time_str, minimize_params['trial'], raw_filename)
